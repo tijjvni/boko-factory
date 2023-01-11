53a65b8abdec31349fa26035409673369c61d050
tijjani-yusuf <116069343+tijjani-yusuf@users.noreply.github.com>
2023-01-11 12:39:49 +0100

    logo path chngd

--- resources/views/vendor/jetstream/components/application-logo.blade.php
+++ resources/views/vendor/jetstream/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img class="w-16 h-16" src="{{ public_path('logo.png') }}">
+<img class="w-16 h-16" src="{{ asset('logo.png') }}">
--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <img  class="w-16 h-16" src="{{ public_path('logo.png') }}">     
+    <img  class="w-16 h-16" src="{{ asset('logo.png') }}">     
 </a>
